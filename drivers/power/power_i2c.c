RaitoBezarius/uboot-nwa50ax

commit 2684ce7301af6520f09cd1147431c8a690cc6100

power: change from meaningless value to error number
'-1' is absolutely meaningless value.
This patch changed from meaningless value to error number.

Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

drivers/power/power_i2c.c
@@ -19,9 +19,10 @@
 int pmic_reg_write(struct pmic *p, u32 reg, u32 val)
 {
 	unsigned char buf[4] = { 0 };
+	int ret;
 
 	if (check_reg(p, reg))
-		return -1;
+		return -EINVAL;
 
 	I2C_SET_BUS(p->bus);
 
@@ -51,27 +52,26 @@ int pmic_reg_write(struct pmic *p, u32 reg, u32 val)
 		break;
 	default:
 		printf("%s: invalid tx_num: %d", __func__, pmic_i2c_tx_num);
-		return -1;
+		return -EINVAL;
 	}
 
-	if (i2c_write(pmic_i2c_addr, reg, 1, buf, pmic_i2c_tx_num))
-		return -1;
-
-	return 0;
+	return i2c_write(pmic_i2c_addr, reg, 1, buf, pmic_i2c_tx_num);
 }
 
 int pmic_reg_read(struct pmic *p, u32 reg, u32 *val)
 {
 	unsigned char buf[4] = { 0 };
 	u32 ret_val = 0;
+	int ret;
 
 	if (check_reg(p, reg))
-		return -1;
+		return -EINVAL;
 
 	I2C_SET_BUS(p->bus);
 
-	if (i2c_read(pmic_i2c_addr, reg, 1, buf, pmic_i2c_tx_num))
-		return -1;
+	ret = i2c_read(pmic_i2c_addr, reg, 1, buf, pmic_i2c_tx_num);
+	if (ret)
+		return ret;
 
 	switch (pmic_i2c_tx_num) {
 	case 3:
@@ -93,7 +93,7 @@ int pmic_reg_read(struct pmic *p, u32 reg, u32 *val)
 		break;
 	default:
 		printf("%s: invalid tx_num: %d", __func__, pmic_i2c_tx_num);
-		return -1;
+		return -EINVAL;
 	}
 	memcpy(val, &ret_val, sizeof(ret_val));
 
@@ -106,7 +106,7 @@ int pmic_probe(struct pmic *p)
 	debug("Bus: %d PMIC:%s probed!\n", p->bus, p->name);
 	if (i2c_probe(pmic_i2c_addr)) {
 		printf("Can't find PMIC:%s\n", p->name);
-		return -1;
+		return -ENODEV;
 	}
 
 	return 0;

drivers/power/axp152.c
@@ -75,7 +75,7 @@ int axp_init(void)
 		return rc;
 
 	if (ver != 0x05)
-		return -1;
+		return -EINVAL;
 
 	return 0;
 }

drivers/power/axp209.c
@@ -158,7 +158,7 @@ int axp_init(void)
 	ver &= 0x0f;
 
 	if (ver != 0x1)
-		return -1;
+		return -EINVAL;
 
 	/* Mask all interrupts */
 	for (i = AXP209_IRQ_ENABLE1; i <= AXP209_IRQ_ENABLE5; i++) {

drivers/power/battery/bat_trats2.c
@@ -18,7 +18,7 @@ static int power_battery_charge(struct pmic *bat)
 	struct power_battery *p_bat = bat->pbat;
 
 	if (bat->chrg->chrg_state(p_bat->chrg, PMIC_CHARGER_ENABLE, 450))
-		return -1;
+		return -EINVAL;
 
 	return 0;
 }

drivers/power/exynos-tmu.c
@@ -190,7 +190,7 @@ static int get_tmu_fdt_values(struct tmu_info *info, const void *blob)
 				      COMPAT_SAMSUNG_EXYNOS_TMU);
 	if (node < 0) {
 		debug("EXYNOS_TMU: No node for tmu in device tree\n");
-		return -1;
+		return -ENODEV;
 	}
 
 	/*
@@ -202,7 +202,7 @@ static int get_tmu_fdt_values(struct tmu_info *info, const void *blob)
 	addr = fdtdec_get_addr(blob, node, "reg");
 	if (addr == FDT_ADDR_T_NONE) {
 		debug("%s: Missing tmu-base\n", __func__);
-		return -1;
+		return -ENODEV;
 	}
 	info->tmu_base = (struct exynos5_tmu_reg *)addr;
 
@@ -246,11 +246,11 @@ static int get_tmu_fdt_values(struct tmu_info *info, const void *blob)
 
 	if (error) {
 		debug("fail to get tmu node properties\n");
-		return -1;
+		return -EINVAL;
 	}
 #else
 	/* Non DT support may never be added. Just in case  */
-	return -1;
+	return -ENODEV;
 #endif
 
 	return 0;

drivers/power/fuel_gauge/fg_max17042.c
@@ -199,7 +199,7 @@ static int power_update_battery(struct pmic *p, struct pmic *bat)
 
 	if (pmic_probe(p)) {
 		puts("Can't find max17042 fuel gauge\n");
-		return -1;
+		return -ENODEV;
 	}
 
 	ret |= pmic_reg_read(p, MAX17042_VFSOC, &val);
@@ -224,7 +224,7 @@ static int power_check_battery(struct pmic *p, struct pmic *bat)
 
 	if (pmic_probe(p)) {
 		puts("Can't find max17042 fuel gauge\n");
-		return -1;
+		return -ENODEV;
 	}
 
 	ret |= pmic_reg_read(p, MAX17042_STATUS, &val);

drivers/power/mfd/fg_max77693.c
@@ -52,7 +52,7 @@ static int power_update_battery(struct pmic *p, struct pmic *bat)
 
 	if (pmic_probe(p)) {
 		puts("Can't find max77693 fuel gauge\n");
-		return -1;
+		return -ENODEV;
 	}
 
 	ret = max77693_get_soc(&pb->bat->state_of_chrg);
@@ -74,7 +74,7 @@ static int power_check_battery(struct pmic *p, struct pmic *bat)
 
 	if (pmic_probe(p)) {
 		puts("Can't find max77693 fuel gauge\n");
-		return -1;
+		return -ENODEV;
 	}
 
 	ret = pmic_reg_read(p, MAX77693_STATUS, &val);

drivers/power/mfd/pmic_max77693.c
@@ -16,7 +16,7 @@ static int max77693_charger_state(struct pmic *p, int state, int current)
 	unsigned int val;
 
 	if (pmic_probe(p))
-		return -1;
+		return -ENODEV;
 
 	/* unlock write capability */
 	val = MAX77693_CHG_UNLOCK;
@@ -27,13 +27,13 @@ static int max77693_charger_state(struct pmic *p, int state, int current)
 		pmic_reg_read(p, MAX77693_CHG_CNFG_00, &val);
 		val &= ~0x01;
 		pmic_reg_write(p, MAX77693_CHG_CNFG_00, val);
-		return -1;
+		return -ENOTSUPP;
 	}
 
 	if (current < CHARGER_MIN_CURRENT || current > CHARGER_MAX_CURRENT) {
 		printf("%s: Wrong charge current: %d [mA]\n",
 		       __func__, current);
-		return -1;
+		return -EINVAL;
 	}
 
 	/* set charging current */
@@ -59,7 +59,7 @@ static int max77693_charger_bat_present(struct pmic *p)
 	unsigned int val;
 
 	if (pmic_probe(p))
-		return -1;
+		return -ENODEV;
 
 	pmic_reg_read(p, MAX77693_CHG_INT_OK, &val);
 

commit cd9c9eb647ca440ddddf4f4df8f190f3abf5f60f

power_i2c.c: Fix unused variable warning
The variable ret was added but never set as we did not make calls to
other functions that we needed to check the return value on.

Fixes: 2684ce7301af ("power: change from meaningless value to error number")
Signed-off-by: Tom Rini <trini@konsulko.com>

drivers/power/power_i2c.c
@@ -19,7 +19,6 @@
 int pmic_reg_write(struct pmic *p, u32 reg, u32 val)
 {
 	unsigned char buf[4] = { 0 };
-	int ret;
 
 	if (check_reg(p, reg))
 		return -EINVAL;