RaitoBezarius/uboot-nwa50ax

commit 9c851af2634e16a459dad8d66679e7a7f6e5015d

log: add category LOGC_EFI
The EFI implementation does not fit into any of the existing categories.

Provide LOGC_EFI so that EFI related message can be filtered.

Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
Reviewed-by: Simon Glass <sjg@chromium.org>

include/log.h
@@ -46,6 +46,7 @@ enum log_category_t {
 	LOGC_CORE,
 	LOGC_DM,	/* Core driver-model */
 	LOGC_DT,	/* Device-tree */
+	LOGL_EFI,	/* EFI implementation */
 
 	LOGC_COUNT,
 	LOGC_END,

common/log.c
@@ -21,6 +21,7 @@ static const char *log_cat_name[LOGC_COUNT - LOGC_NONE] = {
 	"core",
 	"driver-model",
 	"device-tree",
+	"efi",
 };
 
 static const char *log_level_name[LOGL_COUNT] = {

doc/README.log
@@ -51,6 +51,7 @@ The following main categories are defined:
    LOGC_BOARD	- Related to board-specific code
    LOGC_CORE	- Related to core driver-model support
    LOGC_DT	- Related to device tree control
+   LOGC_EFI	- Related to EFI implementation
 
 
 Enabling logging

commit e03ebdffa8dfaf9e7a995181190a4b937c1a0151

log: fix typo LOGL_EFI
According to the documentation the EFI log category is called LOGC_EFI.
All other categories start with LOGC_. So let's fix it.

Fixes: 9c851af2634e ("log: add category LOGC_EFI")
Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
Reviewed-by: Simon Glass <sjg@chromium.org>
Signed-off-by: Alexander Graf <agraf@suse.de>

include/log.h
@@ -46,7 +46,7 @@ enum log_category_t {
 	LOGC_CORE,
 	LOGC_DM,	/* Core driver-model */
 	LOGC_DT,	/* Device-tree */
-	LOGL_EFI,	/* EFI implementation */
+	LOGC_EFI,	/* EFI implementation */
 
 	LOGC_COUNT,
 	LOGC_END,